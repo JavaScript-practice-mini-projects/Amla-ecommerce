c336441b8de1ac4fa22f95e95cac6c8f93b5ebd6
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2024-12-15 11:01:10 +0600

    latest product section

--- Front-End/Javascript/index.js
+++ Front-End/Javascript/index.js
@@ -2,12 +2,17 @@
 // Globals
 const productsDisplay = document.getElementById('products')
 let allProducts = []
+let headerSliderTime = 3000
+let headerSliderIndex = 0
+
 
 window.onload = () => {
     main()
 
     // default calls 
     getData()
+    headerImageSlider()
+    setInterval(headerImageSlider, headerSliderTime)
 }
 
 function main(){
@@ -67,21 +72,75 @@ function dynamicProducts(product){
     `
 }
 
+function latestProductsDisplay(products){
+    const latestProducts = document.getElementById('latestProducts')
+    products.forEach((product) => {
+        console.log(product)
+        const {category, description, id, image, price, rating, title} = product
+        const shortTitle = title.slice(0, 30) + '...'
+        const star = rating.rate
+        const review = rating.count
+    
+    
+        latestProducts.innerHTML += `
+    
+        <div class="product">
+        <img src="${image}" alt="">
+        <h4>${shortTitle}</h4>
+        <div class="rating">
+            <div class="star"><i class="fa-solid fa-star"></i><span>${star}</span></div>
+            <div class="review"><span>${review} Reviews</span></div>
+        </div>
+        <span>$${price}</span>
+        </div>
+        `
+    })
+
+
+    
+
+}
+
+
+function headerSlider(image){
+    const headerImagesSlider = document.getElementById('headerImagesSlider')
+    headerImagesSlider.innerHTML = ''
+    // create images container
+    const div = document.createElement('div')
+    div.classList.add('header-images-slider')
+    
+    // create images element
+    const img = document.createElement('img')
+    img.src = image[headerSliderIndex].img
+
+    div.appendChild(img)
+    headerImagesSlider.appendChild(div)
+
+    if(headerSliderIndex < image.length -1){
+        headerSliderIndex ++
+    }else{
+        headerSliderIndex = 0
+    }
+    
+}
 
 
 //Units Function
 
 /**
- * api data fetching
+ * api products data fetching
  */
 async function getData(){
-    const url = 'https://fakestoreapi.com/products'
+    const url = '../../Back-End/Data/products.json'
     try{
         const response = await fetch(url)
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status}`)
         }
         const data = await response.json()
+        const latest = data.slice(-5)
+        latestProductsDisplay(latest)
+
         const products = await data
         productsDisplay.innerHTML = '';
         products.forEach((product) => {
@@ -99,4 +158,22 @@ function searchProductsDisplay(searchResults){
     searchResults.forEach((product) => {
         dynamicProducts(product)
     })
+}
+
+
+/**
+ * header slider images display
+ */
+async function headerImageSlider(){
+    const url = '../../Back-End/Data/headerSliderImages.json'
+    try{
+        const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        headerSlider(data)
+    }catch(error){
+        console.error("Error fetching data", error.message)
+    }
 }
